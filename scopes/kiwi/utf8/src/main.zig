f386f543cc51b0540602ac2ad3ee79b9ca701535
Sheel Patel <hello@sheel.me>
2023-10-03 14:20:10 +0000

    update isAscii to take a slice of bytes

--- scopes/kiwi/utf8/src/main.zig
+++ scopes/kiwi/utf8/src/main.zig
@@ -2,16 +2,53 @@ const std = @import("std");
 const testing = std.testing;
 const simd = std.simd;
 
-const Vector = @Vector(16, u8);
+/// Returns true if the given bytes are all ASCII. \
+/// precondition: N > 0
+pub fn isAscii(comptime N: usize, bytes: []const u8) bool {
+    comptime {
+        if (N == 0) @compileError("N can't be zero");
+    }
 
-fn isAscii(vector: Vector) bool {
+    const Vector = @Vector(N, u8);
+
     const mask: Vector = @splat(0x80);
     const zeroes: Vector = @splat(0);
-    return @reduce(.And, (vector & mask) == zeroes);
+    var it = std.mem.window(u8, bytes, N, N);
+    while (it.next()) |window| {
+        if (window.len < N) break;
+        if (!@reduce(.And, (window[0..N].* & mask) == zeroes)) return false;
+    }
+
+    var paddedVector: Vector = zeroes;
+    const remainder = bytes[bytes.len - (bytes.len % N) ..];
+    const paddedArray: [*]u8 = @ptrCast(&paddedVector);
+    std.mem.copy(u8, paddedArray[0..remainder.len], remainder);
+    return @reduce(.And, (paddedVector & mask) == zeroes);
 }
 
-test "validate ASCII characters" {
-    try testing.expectEqual(true, isAscii(simd.iota(u8, @sizeOf(Vector))));
-    try testing.expectEqual(true, isAscii(@splat(127)));
-    try testing.expectEqual(false, isAscii(@splat(128)));
+test "isAscii" {
+    const expect = std.testing.expect;
+
+    // Boundary conditions
+    try expect(isAscii(1, &[_]u8{0}) == true);
+    try expect(isAscii(1, &[_]u8{127}) == true);
+    try expect(isAscii(1, &[_]u8{128}) == false);
+
+    // Empty input
+    try expect(isAscii(1, &[_]u8{}) == true);
+
+    // Input that is a multiple of N
+    try expect(isAscii(4, &[_]u8{ 65, 66, 67, 68 }) == true);
+    try expect(isAscii(4, &[_]u8{ 125, 126, 127, 128 }) == false);
+    try expect(isAscii(4, &[_]u8{ 125, 126, 128, 127 }) == false);
+    try expect(isAscii(4, &[_]u8{ 125, 128, 126, 127 }) == false);
+    try expect(isAscii(4, &[_]u8{ 128, 125, 126, 127 }) == false);
+
+    // Input that is not a multiple of N
+    try expect(isAscii(4, &[_]u8{ 65, 66, 67, 68, 69 }) == true);
+    try expect(isAscii(4, &[_]u8{ 124, 125, 126, 127, 128 }) == false);
+    try expect(isAscii(4, &[_]u8{ 124, 125, 126, 128, 127 }) == false);
+    try expect(isAscii(4, &[_]u8{ 124, 125, 128, 126, 127 }) == false);
+    try expect(isAscii(4, &[_]u8{ 124, 128, 125, 126, 127 }) == false);
+    try expect(isAscii(4, &[_]u8{ 128, 124, 125, 126, 127 }) == false);
 }
